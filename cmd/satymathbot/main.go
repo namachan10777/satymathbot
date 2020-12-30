1a3a02215e7fd7edab343bed9f96791b58ef90a3
Nakano Masaki <namachan10777@gmail.com>
2020-12-30 23:32:37 +0900

    docker app

--- cmd/satymathbot/main.go
+++ cmd/satymathbot/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os/exec"
+)
 
 func main() {
 	fmt.Println("Hello World!")
+	satysfi_out, satysfi_err := exec.Command("/usr/local/bin/satysfi", "--help").Output()
+	if satysfi_err != nil {
+		log.Fatal(satysfi_err)
+	}
+	fmt.Printf("satysfi: %s\n", satysfi_out)
+	pdftoppm_out, pdftoppm_err := exec.Command("/usr/bin/pdftoppm", "--help").Output()
+	if pdftoppm_err != nil {
+		log.Fatal(pdftoppm_err)
+	}
+	fmt.Printf("pdftoppm: %s\n", pdftoppm_out)
 }
