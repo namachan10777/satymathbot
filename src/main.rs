d33de07d26c15e4bd110b0a46295f5d9753abd82
Masaki Nakano <admin@namachan10777.dev>
2021-12-11 19:23:44 +0800

    parse arguments

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,26 @@
+use clap::StructOpt;
 use tracing::info;
+
+#[derive(clap::Parser)]
+#[clap(about, version, author)]
+struct Parser {
+    #[clap(short, long)]
+    workdir: String,
+    #[clap(short = 's', long)]
+    style_file: Option<String>,
+    #[clap(short = 'b', long)]
+    satysfi_bin: String,
+}
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
-    info!("Hello World!");
+    let opts = Parser::parse();
+    let style_file = opts
+        .style_file
+        .unwrap_or_else(|| format!("{}/satymathbot.satyh", opts.workdir));
+    info!(
+        "Start {}, {}, {}",
+        opts.satysfi_bin, style_file, opts.workdir
+    );
 }
