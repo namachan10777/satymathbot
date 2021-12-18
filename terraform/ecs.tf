183b60ed9796d257fa363a0c7616d58319202f21
Masaki Nakano <admin@namachan10777.dev>
2021-12-18 16:03:31 +0800

    create alb listener

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -12,9 +12,9 @@ resource "aws_security_group" "alb" {
   vpc_id      = data.aws_vpc.default.id
 
   ingress {
-    description      = "TLS from anywhere"
-    from_port        = 443
-    to_port          = 443
+    description      = "http from anywhere"
+    from_port        = 80
+    to_port          = 80
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
@@ -29,4 +29,27 @@ resource "aws_alb" "main" {
   subnets            = data.aws_subnet_ids.default.ids
 
   enable_deletion_protection = true
+}
+
+resource "aws_lb_target_group" "main" {
+  name        = "satymathbot"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = data.aws_vpc.default.id
+  target_type = "ip"
+  health_check {
+    port = 80
+    path = "/health"
+  }
+}
+
+resource "aws_lb_listener" "main" {
+  load_balancer_arn = aws_alb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_lb_target_group.main.arn
+    type             = "forward"
+  }
 }
