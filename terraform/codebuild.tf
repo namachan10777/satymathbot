f2dad17487cc420dea22e3a9dacfa0346ba6dc54
Masaki Nakano <admin@namachan10777.dev>
2021-12-20 03:06:40 +0800

    ?

--- terraform/codebuild.tf
+++ terraform/codebuild.tf
@@ -25,6 +25,26 @@ data "aws_iam_policy_document" "codebuild" {
     ]
   }
   statement {
+    effect    = "Allow"
+    resources = ["*"]
+    actions = [
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:GetRepositoryPolicy",
+      "ecr:DescribeRepositories",
+      "ecr:ListImages",
+      "ecr:DescribeImages",
+      "ecr:BatchGetImage",
+      "ecr:ListTagsForResource",
+      "ecr:DescribeImageScanFindings",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload",
+      "ecr:PutImage"
+    ]
+  }
+  statement {
     resources = [
       "arn:aws:logs:ap-northeast-1:966924987919:log-group:/aws/codebuild/satymathbot:*"
     ]
