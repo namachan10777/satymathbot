ea9b909d36973f101c2a969fcd98980e7cbe0665
Masaki Nakano <admin@namachan10777.dev>
2021-12-20 23:48:10 +0800

    unix socket

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,10 +1,11 @@
-use std::sync::Arc;
-use std::{net::SocketAddr, process::exit};
-
 use axum::{response::IntoResponse, routing, Json};
 use clap::StructOpt;
+use hyperlocal::UnixServerExt;
 use serde::{Deserialize, Serialize};
-use tokio::fs;
+use std::path::Path;
+use std::process::exit;
+use std::sync::Arc;
+use tokio::{fs, io};
 use tracing::{error, info};
 
 #[derive(clap::Parser)]
@@ -29,6 +30,7 @@ pub struct Config {
     pub workdir: String,
     pub capacity: usize,
     pub sock_path: String,
+    pub healthcheck_sock_path: String,
 }
 
 impl Default for Config {
@@ -40,6 +42,7 @@ impl Default for Config {
             workdir: "/tmp/satymathbot".to_owned(),
             capacity: 4096,
             sock_path: "/var/run/saytmathbot/app.sock".to_owned(),
+            healthcheck_sock_path: "/var/run/satymathbot/app_health.sock".to_owned(),
         }
     }
 }
@@ -89,6 +92,74 @@ async fn health() -> impl IntoResponse {
     })
 }
 
+async fn prepare_env(cfg: &Config) {
+    if let Err(e) = satymathbot::prepare(&cfg.satyh, &cfg.workdir).await {
+        error!("Cannot prepare environment due to {:?}", e);
+        exit(-1);
+    } else {
+        info!("Prepared environment")
+    }
+}
+
+async fn clear_socket<P>(app: P, health: P) -> io::Result<()>
+where
+    P: AsRef<Path>,
+{
+    if app.as_ref().exists() {
+        fs::remove_file(app).await?;
+    }
+    if health.as_ref().exists() {
+        fs::remove_file(health).await?;
+    }
+    Ok(())
+}
+
+async fn run_server(cfg: Config) {
+    let state = Arc::new(satymathbot::State::new(cfg.clone().into()));
+    clear_socket(&cfg.sock_path, &cfg.healthcheck_sock_path)
+        .await
+        .unwrap_or_else(|e| {
+            error!("Cannot clean up sockets due to {}", e);
+            exit(-1);
+        });
+    let app = axum::Router::new()
+        .route("/:file", routing::get(satymathbot::endpoint))
+        .route("/health", routing::get(health))
+        .layer(axum::AddExtensionLayer::new(state));
+    let health = axum::Router::new().route("/", routing::get(health));
+    info!("Listen on {}", cfg.sock_path);
+    let healthcheck_sock_path = cfg.healthcheck_sock_path.to_owned();
+    tokio::spawn(async move {
+        if let Err(e) = axum::Server::bind_unix(healthcheck_sock_path.clone())
+            .unwrap_or_else(|e| {
+                error!(
+                    "Cannot bind unix socket {} due to {}",
+                    healthcheck_sock_path, e
+                );
+                exit(-1);
+            })
+            .serve(health.into_make_service())
+            .with_graceful_shutdown(shutdown_signal())
+            .await
+        {
+            error!("Health server exited accidently: {:?}", e);
+            exit(-1);
+        }
+    });
+    if let Err(e) = axum::Server::bind_unix(&cfg.sock_path)
+        .unwrap_or_else(|e| {
+            error!("Cannot bind unix socket {} due to {}", cfg.sock_path, e);
+            exit(-1);
+        })
+        .serve(app.into_make_service())
+        .with_graceful_shutdown(shutdown_signal())
+        .await
+    {
+        error!("Server exited accidently: {:?}", e);
+        exit(-1);
+    }
+}
+
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
@@ -103,26 +174,8 @@ async fn main() {
                 error!("Invalid config file ({:?})", e);
                 exit(-1);
             });
-            let state = Arc::new(satymathbot::State::new(cfg.clone().into()));
-            if let Err(e) = satymathbot::prepare(&cfg.satyh, &cfg.workdir).await {
-                error!("Cannot prepare environment due to {:?}", e);
-            } else {
-                info!("Prepared environment")
-            }
-
-            let app = axum::Router::new()
-                .route("/:file", routing::get(satymathbot::endpoint))
-                .route("/health", routing::get(health))
-                .layer(axum::AddExtensionLayer::new(state));
-            let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
-            info!("Listen on {}", addr);
-            if let Err(e) = axum::Server::bind(&addr)
-                .serve(app.into_make_service())
-                .with_graceful_shutdown(shutdown_signal())
-                .await
-            {
-                error!("Server exited accidently: {:?}", e);
-            }
+            prepare_env(&cfg).await;
+            run_server(cfg).await;
         }
         None => {
             let pretty_cfg = ron::ser::PrettyConfig::new();
