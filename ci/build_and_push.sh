e437a56aa9f214d1ef4bea9754577e1d5c6d93a9
Masaki Nakano <admin@namachan10777.dev>
2021-12-25 03:21:28 +0800

    improve build script

--- ci/build_and_push.sh
+++ ci/build_and_push.sh
@@ -4,19 +4,19 @@ set -eux
 
 ECR_REGISTRY="966924987919.dkr.ecr.ap-northeast-1.amazonaws.com"
 REPOS=("web" "envoy" "app" "promtail" "prometheus")
+REVISION="$(git rev-parse HEAD)"
 
 function build_and_push {
     local repo="${ECR_REGISTRY}/satymathbot-$1"
-    local revision="$(git rev-parse HEAD)"
     docker pull "${repo}":latest || true
     docker build \
         --cache-from "${repo}:latest" \
         --build-arg BUILDKIT_INLINE_CACHE=1 \
         -t "${repo}:latest" \
         "$1" 
-    docker tag "${repo}:latest" "${repo}:${revision}"
+    docker tag "${repo}:latest" "${repo}:${REVISION}"
     docker push "${repo}:latest"
-    docker push "${repo}:${revision}"
+    docker push "${repo}:${REVISION}"
 }
 
 for repo in "${REPOS[@]}"; do
